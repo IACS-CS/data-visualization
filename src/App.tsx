440524c96aca7601a99a61693473893b1dbf558d
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-02-14 12:30:06 +0000

    Basic starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,70 @@
 import { useEffect, useState } from "react";
+import papa from 'papaparse'
 import "./App.css";
-import ImageFilterChooser from "./components/ImageFilterChooser";
-import filters from "./filters";
-import type { Filter, ImageInfo } from "./types";
-import { images } from "./images";
-import { ImageDisplay } from "./components/ImageDisplay";
-import { FilteredImageDisplay } from "./components/FilteredImageDisplay";
-import ImageSelector from "./components/ImageSelector";
-import FilterOptionComponent from "./components/FilterOption";
-const defaultImage = images[0];
+import type { DemoDataRow, PieDataRow } from "./types";
+import { PieChart, Pie, Cell, LabelList } from "recharts";
+
 const App = () => {
-  const [filter, setFilter] = useState<Filter | null>(filters[0]);
-  const [image, setImage] = useState<ImageInfo>(defaultImage);
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
-  const [filterOptions, setFilterOptions] = useState<{
-    [key: string]: boolean | string | number;
-  }>({});
+  const [csvData,setCsvData] = useState<DemoDataRow[]>([]);
+  const [pieData,setPieData] = useState<PieDataRow[]>([]);
+  const csvFileUrl = '/data/demo.csv'; // FIX ME
 
-  useEffect(() => {
-    let newOptions: { [key: string]: boolean | string | number } = {};
-    if (filter && filter.options) {
-      for (let o of filter.options) {
-        newOptions[o.name] = o.default;
+  const getData = async () => {
+    let response = await fetch(csvFileUrl);
+    let text = await response.text();
+    let parsed = await papa.parse<DemoDataRow>(text,{header:true});
+    console.log('Successfully parsed data:',parsed); // Log to make it easy to inspect shape of our data in the inspector
+    setCsvData(parsed.data.filter((row)=>row.Name)); // Only keep rows that have a name, so we avoid blank row at end of file
+  }
+
+
+
+
+  useEffect(
+    ()=>{getData()},[]
+  );
+
+  useEffect(
+    ()=>{
+      // Update whenever data changes...
+      let newPieCounts : {[key : string] : number} = {};
+      let newPieData : PieDataRow[] = [];
+      csvData.forEach(
+        (row)=>{
+          if (!newPieCounts[row["Favorite Color"]]) {
+            newPieCounts[row["Favorite Color"]] = 0; // initialize if not there...
+          }
+          newPieCounts[row["Favorite Color"]]++ // Add one!
+        }
+      )
+      for (let key in newPieCounts) {
+        newPieData.push(
+          {name : key, value : newPieCounts[key]}
+        )
       }
+      setPieData(newPieData);
+      console.log('Set new pie data!',newPieData)
     }
-    setFilterOptions(newOptions);
-  }, [filter]);
+  ,[csvData])
 
-  const getSize = (width: number, height: number) => {
-    setWidth(width);
-    setHeight(height);
-  };
-
-  const setFilterOption = (name: string, value: boolean | string | number) => {
-    setFilterOptions({ ...filterOptions, [name]: value });
-  };
 
   return (
     <main>
-      <nav>
-        <div>
-          Filter: <ImageFilterChooser onInput={setFilter} />
-        </div>
-        <div>
-          Image:
-          <ImageSelector onImageSelect={setImage} currentImage={image} />
-        </div>
-      </nav>
-      <h1>
-        Filtering {image.name} image &nbsp;
-        {(width && (
-          <div className="size">
-            ({width}x{height})
-          </div>
-        )) ||
-          ""}{" "}
-      </h1>
-      <section className="options">
-        {filter &&
-          filter.options &&
-          filter.options.map((option) => (
-            <FilterOptionComponent
-              currentValue={filterOptions[option.name]}
-              option={option}
-              onInput={setFilterOption}
-            />
-          ))}
-      </section>
-      <section className="side-by-side">
-        <div>
-          <h2>Original</h2>
-          {image ? <ImageDisplay image={image} /> : "No image selected..."}
-        </div>
-        <div>
-          <h2>Filtered</h2>
-          {filter && image ? (
-            <FilteredImageDisplay
-              image={image}
-              filter={filter}
-              filterOptions={filterOptions}
-              onImageLoad={getSize}
-            />
-          ) : (
-            "waiting on filter/image..."
+      <h1>Hello Data Visualization</h1>
+      <p>Loaded {csvData.length} rows of CSV Data!</p>
+      <PieChart width={800} height={800}>
+        <Pie data={pieData} dataKey="value" nameKey="name" label fill="yellow">
+          <LabelList dataKey="name" position="middle"/>
+          {
+          pieData.map(
+            (entry)=>(<Cell fill={entry.name.toLowerCase()} />)
           )}
-        </div>
-      </section>
+        </Pie>
+
+      </PieChart>
+      {csvData.map(
+        (row)=><div>{row.Name} age {row.Age}'s favorite color is {row["Favorite Color"]} and they play {row["Favorite Sport"]}</div>
+      )}
     </main>
   );
 };
